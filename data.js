ca40e04bf1e86a3d6e5c9f0eba7e2559b637232a
Horsty <derberdreck69@gmail.com>
2016-02-27 18:40:37 +0100

    Changed the location to Düsseldorf and added three locations. Made a data input page for students

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 
 var punkte = [
 
-  { geo: [51.5, -0.09],
+  { geo: [51.249117, 6.732633],
     name: "Tomatenwachstum",
     wer: "Mariengymnasium Klasse 5c",
     icon: "tomato",
     datum: '2015-11-03' },
 
-    { geo: [51.51, -0.08],
+    { geo: [51.248981, 6.746900],
       name: "Körpergröße",
       wer: " Klasse 5c",
       icon: "person",
     datum: '2015-12-11' },
 
 
-  { geo: [51.515, -0.085],
+  { geo: [51.271635, 6.734526],
     name: "Chemiekurs",
     wer: "Kinder-Uni",
     icon: "chemie",
